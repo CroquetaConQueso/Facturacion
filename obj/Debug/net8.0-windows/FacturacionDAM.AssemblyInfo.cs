88a42519711493c1078b4f1e704ad1792ad57c37
CroquetaConQueso <lacio161@gmail.com>
2026-02-14 15:04:31 +0100

    Anual

--- obj/Debug/net8.0-windows/FacturacionDAM.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/FacturacionDAM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FacturacionDAM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df97189306efcccd9c286b8a8be10fce8c123e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a8bdf41111b4de32b43183bcf91c09a5069895")]
 [assembly: System.Reflection.AssemblyProductAttribute("FacturacionDAM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FacturacionDAM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
